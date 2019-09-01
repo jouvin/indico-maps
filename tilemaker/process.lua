d68eef30f05e9838e28a31f8344045276b6b3a0f
root <root@docker1.lal.in2p3.fr>
2019-09-01 15:34:16 +0200

    Revert "tilemaker: remove use of the shapefile defining FLUO buildings area"
    - FLUO shapefile fixed
    
    This reverts commit 79d3b4342f50b9b2cff2c71eea424a4b39eac18e.

--- tilemaker/process.lua
+++ tilemaker/process.lua
@@ -36,11 +36,15 @@ function way_function(way)
     end
     if building~="" then
         local name = way:Find("name")
-        if name ~= "" then
-            way:LayerAsCentroid("building_names")
-            way:Attribute("name", name)
+        if way:Intersects("fluo_sites") then
+            way:Layer("fluo_building", true)
+            if name ~= "" then
+                way:LayerAsCentroid("building_names")
+                way:Attribute("name", name)
+            end
+        else
+            way:Layer("building", true)
         end
-        way:Layer("building", true)
     end
     if amenity == "research_institute" then
         way:Layer("limits", false)
